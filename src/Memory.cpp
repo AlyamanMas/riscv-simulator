6a6337a5b4de05519ef5afa4f0efac602858e395
Alyaman Massarani <alyaman.maasarani@gmail.com>
2023-11-18 19:37:08 +0200

    Implemented some of the Memory class

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "Memory.h"
+
+void Memory::set_byte(uint32_t address, uint8_t value) {
+    memory_map[address] = value;
+}
+
+void Memory::set_half_word(uint32_t address, uint16_t value) {
+    memory_map[address] = value & 0xFF;
+    memory_map[address + 1] = (value >> 8) & 0xFF;
+}
+
+void Memory::set_word(uint32_t address, uint32_t value) {
+    memory_map[address] = value & 0xFF;
+    memory_map[address + 1] = (value >> 8) & 0xFF;
+    memory_map[address + 2] = (value >> 16) & 0xFF;
+    memory_map[address + 3] = (value >> 24) & 0xFF;
+}
+
+uint8_t Memory::get_byte(uint32_t address) const {
+    return memory_map.at(address);
+}
+
+uint16_t Memory::get_half_word(uint32_t address) const {
+    return (memory_map.at(address + 1) << 8) |
+           memory_map.at(address);
+}
+
+uint32_t Memory::get_word(uint32_t address) const {
+    return (memory_map.at(address + 3) << 24) |
+           (memory_map.at(address + 2) << 16) |
+           (memory_map.at(address + 1) << 8) |
+           memory_map.at(address);
+}
--- src/Memory.h
+++ src/Memory.h
@@ -14,26 +14,33 @@ class Memory {
 public:
     typedef uint32_t Address_t;
 
-    Memory();
+    Memory() = default;
+
     Memory(std::string memory_text);
 
-    uint32_t get_word(uint32_t address);
-    uint16_t get_half_word(uint32_t address);
-    uint8_t get_byte(uint32_t address);
+    uint32_t get_word(uint32_t address) const;
+
+    uint16_t get_half_word(uint32_t address) const;
+
+    uint8_t get_byte(uint32_t address) const;
 
     void set_word(uint32_t address, uint32_t value);
+
     void set_half_word(uint32_t address, uint16_t value);
+
     void set_byte(uint32_t address, uint8_t value);
 
-    std::string serialize();
+//    std::string serialize();
+    const std::map<Address_t, uint8_t> &get_const_reference() { return memory_map; };
 
 private:
     typedef std::tuple<Address_t, std::variant<uint32_t, uint16_t, uint8_t, std::string>> MemoryOperation_t;
+
     std::map<Address_t, uint8_t> memory_map;
 
     MemoryOperation_t deserialize_memory_operation(std::string memory_operation_text);
-    void execute_memory_operation(MemoryOperation_t memory_operation);
 
+    void execute_memory_operation(MemoryOperation_t memory_operation);
 };
 
 
