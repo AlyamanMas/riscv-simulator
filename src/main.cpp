1aff86be19aa746577e4cf72235e874dbbaa30a5
Alyaman Massarani <alyaman.maasarani@gmail.com>
2023-11-19 23:14:06 +0200

    Main program works

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@ int main() {
     auto test_file = project_dir / "riscv1.asm";
 
     auto my_program = AssemblyProgram(read_file(test_file));
+    my_program.get_memory().execute_memory_string({"0x1000, word, 0x12345678", "0x1004, word, 0x9abcdef0", "0x1008, string, Hello world, Wow, this is a string"});
     my_program.run();
     return 0;
 }
